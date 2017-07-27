32b05c7a8c49ba387c5d2896c56f0212286d24b7
uncia <uncia@users.noreply.github.com>
2017-07-27 23:26:41 +0800

    Update shadowsocksR.sh

--- shadowsocksR.sh
+++ shadowsocksR.sh
@@ -187,8 +187,8 @@ pre_install(){
 # Download files
 download_files(){
     # Download libsodium file
-    if ! wget --no-check-certificate -O libsodium-1.0.12.tar.gz http://git.oschina.net/uncia/tmp/raw/master/libsodium-1.0.12.tar.gz; then
-        echo "Failed to download libsodium-1.0.12.tar.gz!"
+    if ! wget --no-check-certificate -O libsodium-1.0.13.tar.gz https://github.com/jedisct1/libsodium/releases/download/1.0.13/libsodium-1.0.13.tar.gz; then
+        echo "Failed to download libsodium-1.0.13.tar.gz!"
         exit 1
     fi
     # Download ShadowsocksR file
@@ -333,7 +333,7 @@ install(){
 # Install cleanup
 install_cleanup(){
     cd ${cur_dir}
-    rm -rf manyuser.zip shadowsocksr-manyuser libsodium-1.0.12.tar.gz libsodium-1.0.12
+    rm -rf manyuser.zip shadowsocksr-manyuser libsodium-1.0.13.tar.gz libsodium-1.0.13
 }
 
 
